7d730dd4686117bd5231139a541585cfab0890ac
Christoph Barosch <barosch.christoph@hotmail.com>
2017-06-09 10:50:55 +0200

    Added Unit Tests

---
+++ Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RemoteVolume.Server;
+
+namespace RemoteVolume.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void ChangeVolume()
+        {
+            string command = JsonConvert.SerializeObject(new Command(Server.Action.ChangeVolume, 10, "System Sounds"));
+            Logic.Do(command);
+        }
+    }
+}
