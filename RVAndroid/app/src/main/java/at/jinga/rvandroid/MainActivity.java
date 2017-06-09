4d47191d620185a6aea71362eeed0cfec50f51b6
Christoph Barosch <barosch.christoph@hotmail.com>
2017-06-09 19:39:17 +0200

    Added Android App; Able to connect to Server

---
+++ RVAndroid/app/src/main/java/at/jinga/rvandroid/MainActivity.java
@@ -0,0 +1,47 @@
+package at.jinga.rvandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+    private Socket server;
+    private PrintWriter out;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void connect(View view) {
+        new Thread()
+        {
+            public void run() {
+                EditText ip = (EditText) findViewById(R.id.ip);
+
+                try {
+                    server = new Socket(ip.getText().toString(), 3131);
+
+                    out = new PrintWriter(server.getOutputStream(), true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    public void send(View view) {
+        EditText command = (EditText) findViewById(R.id.command);
+
+        out.println(command.getText().toString());
+    }
+}
