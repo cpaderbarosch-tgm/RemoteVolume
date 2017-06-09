199fbe8399e17bb56e5551e9d79477aaa10ec454
Christoph Barosch <barosch.christoph@hotmail.com>
2017-06-09 23:28:19 +0200

    Applied new gitignore

---
+++ RVServer/Logic.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+namespace RemoteVolume.Server
+{
+    public class Logic
+    {
+        public static void Do(string json)
+        {
+            //Command command = JsonConvert.DeserializeObject<Command>(json);
+
+            //switch (command.Action)
+            //{
+            //    case Action.ChangeVolume:
+            //        VolumeControl.SetApplicationVolume(command.App, command.Volume);
+            //        break;
+            //    case Action.ToggleMute:
+            //        VolumeControl.SetApplicationMute(command.App, (bool) !VolumeControl.GetApplicationMute(command.App));
+            //        break;
+            //}
+            System.Console.WriteLine(json);
+        }
+    }
+
+    public class Command
+    {
+        public Action Action { get; set; }
+
+        private int _volume;
+        public int Volume
+        {
+            get => _volume;
+            set
+            {
+                if (value >= 0 && value <= 100) _volume = value;
+            }
+        }
+
+        public string App { get; set; }
+
+        public Command(Action Action, int Volume, string App)
+        {
+            this.Action = Action;
+            this.Volume = Volume;
+            this.App = App;
+        }
+    }
+
+    public enum Action
+    {
+        ChangeVolume,
+        ToggleMute,
+        Mute,
+        Unmute
+    }
+}
